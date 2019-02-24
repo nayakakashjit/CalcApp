72587c3f6ff9cfa14798625e4ad12f0f5cc7c4cd
nayakakahjit <nayakakashjit@gmail.com>
2019-02-25 00:53:17 +0530

    First commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Calc - App';
+  num1:number;
+  num2:number;
+  result:number;
+  add(){
+    this.result = this.num1 + this.num2;
+  }
+  sub(){
+    this.result = this.num1 - this.num2;
+  }
+  mul(){
+    this.result = this.num1 * this.num2;
+  }
+  div(){
+    this.result = this.num1 / this.num2;
+  }
+}
